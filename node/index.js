e958b4fe67cee3a11b8b7c6ab63a09710de0d713
David Gallmeier <dg@xounges.net>
2023-05-31 15:46:14 -0400

    feat: add swagger ui

--- node/index.js
+++ node/index.js
@@ -1,13 +1,53 @@
 var os = require('os')
 const express = require('express')
 const app = express()
-const port = 3000
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./index.js"],
+};
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.send('Hello World!')
 })
 
+const port = 3000
 app.listen(port, () => {
-  console.log(`hostname: ${os.hostname()}`)
-  console.log(`Example app listening on local ${port}`)
+	console.log(`hostname: ${os.hostname()}`)
+	console.log(`Example app listening on local ${port}`)
 })
